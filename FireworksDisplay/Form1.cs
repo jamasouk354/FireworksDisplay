79c54381745a7c50eb7a86fb18675802c9cd6879
Jamal S <jamasouk354@ed.amdsb.ca>
2018-11-13 14:43:10 -0500

    Add project files.

---
+++ FireworksDisplay/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace FireworksDisplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+
+            Graphics g = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            Pen drawPen = new Pen(Color.White, 10);
+
+
+            int grow = 1;
+            int x = this.Width / 2;
+            int y = this.Height;
+            int fwork = 10;
+
+            while (y > this.Height/2)
+            {
+                g.Clear(Color.Black);
+                g.FillRectangle(drawBrush, x, y, fwork, fwork);
+                Thread.Sleep(10);
+
+                y--;
+            }
+            while (grow < 499)
+            {
+                g.Clear(Color.Black);
+                g.DrawEllipse(drawPen, x - grow / 2, y - grow / 2, fwork + grow, fwork + grow);
+                Thread.Sleep(3);
+
+                grow++;
+            }
+        }
+    }
+}
